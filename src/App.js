5677893861b7736e248a55f1904dc649c912c512
ReeceRenninger <renningerreece@gmail.com>
2023-07-31 11:34:54 -0700

    setting up routes and bringing in material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import Scheduler from './pages/Scheduler';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './pages/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-          Testing dev branch connection
-       
-      </header>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/scheduler" element={<Scheduler />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Scheduler.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const Scheduler = () => { 
+  const [events, setEvents] = useState([]);
+
+  return (
+    <div>
+      <h1>Schedule Game Time</h1>
+    </div>
+  );
+};
+
+export default Scheduler;
