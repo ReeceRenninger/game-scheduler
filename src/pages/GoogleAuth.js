9e61d7a33a9854e23981e0945aca7188cd47ff45
ReeceRenninger <renningerreece@gmail.com>
2023-08-02 14:27:58 -0700

    updated env sample to match correct format, installed react if, signin and signout buttons appear and disappear as needed, still a bug though withe state logic

--- src/pages/GoogleAuth.js
+++ src/pages/GoogleAuth.js
@@ -1,4 +1,7 @@
 import ApiCalendar from 'react-google-calendar-api';
+import { Button } from '@mui/material';
+import { useState } from 'react';
+import { If, Then } from 'react-if';
 
 const clientId = process.env.REACT_APP_CLIENT_ID;
 const apiKey = process.env.REACT_APP_API_KEY;
@@ -14,29 +17,62 @@ const config = {
 
 const apiCalendar = new ApiCalendar(config)
 
-function handleItemClick(event, name) {
-    if (name === 'sign-in') {
-        apiCalendar.handleAuthClick()
-    } else if (name === 'sign-out') {
-        apiCalendar.handleSignoutClick();
-    }
-}
+
 
 const GoogleAuth = () => {
+
+    const [isSignedIn, setIsSignedIn] = useState(false);
+    
+    const  handleSignIn = (event) => {
+        if(!isSignedIn){
+            apiCalendar.handleAuthClick();
+            setIsSignedIn(true);
+        } else {
+            console.log('already signed in');
+        }
+    }
+    
+    const handleSignOut = (event) => {
+        if(isSignedIn){
+            apiCalendar.handleSignoutClick();
+            setIsSignedIn(false);
+        } else {
+            console.log('already signed out');
+        }
+    }
+    
+    // bug with this code, user automatically sees the logout button even if they did not complete the login process
     return (
         <>
-            <button
-                onClick={(e) => handleItemClick(e, 'sign-in')}
-            >
-                sign-in
-            </button>
-            <button
-                onClick={(e) => handleItemClick(e, 'sign-out')}
-            >
-                sign-out
-            </button>
+            <If condition={!isSignedIn}>
+                <Then>
+                    {/* (e) => handleItemClick(e, 'sign-in', setIsSignedIn) */}
+                    <Button variant='contained' color='success' onClick={handleSignIn}>
+                        sign-in
+                    </Button>
+                </Then>
+            </If>
+            <If condition={isSignedIn}>
+                <Then>
+                    {/* (e) => handleItemClick(e, 'sign-out', setIsSignedIn) */}
+                    <Button variant='contained' color='error' onClick={handleSignOut}>
+                        sign-out
+                    </Button>
+                </Then>
+            </If>
         </>
     );
 }
 
 export default GoogleAuth;
+
+
+// function handleItemClick(event, name, setIsSignedIn) {
+//     if (name === 'sign-in') {
+//         apiCalendar.handleAuthClick()
+//         setIsSignedIn(true);
+//     } else if (name === 'sign-out') {
+//         apiCalendar.handleSignoutClick();
+//         setIsSignedIn(false);
+//     }
+// }
